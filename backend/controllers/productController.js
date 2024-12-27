6dde40e5930d0e1ad50fde013056c6bf16086b07
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-12-27 11:26:12 +0530

    API for geting single Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,10 @@
 const ProductModel = require('../models/productModel');
 
+
+//Get Products API - /api/v1/products
 exports.getProducts = async(req, res, next)=>{
 
     const products = await ProductModel.find({})
-
 
     res.json({
         Sucees: true,
@@ -11,9 +12,23 @@ exports.getProducts = async(req, res, next)=>{
     })
 }
 
-exports.getSingleProduct = (req, res, next)=>{
-    res.json({
-        Sucees: true,
-        message: 'Get Single Product Working!'
-    })
+
+//Get Single Producta API - api/v1/product/:id
+exports.getSingleProduct = async(req, res, next)=>{
+    
+    try{
+        const product = await ProductModel.findById(req.params.id);
+
+        res.json({
+            success: true,
+            product
+        })
+    }
+    catch(error){
+        res.status(404).json({
+            success: false,
+            message: 'Unable to get Product with this id'
+        })
+    }
+    
 }
